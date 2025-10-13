0bc683b718467269591d3468d2f129d3207db59d
yourname <your@email.com>
2025-10-14 00:14:09 +0330

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sunny__home/', // Set base for GitHub Pages
   server: {
     historyApiFallback: true,
   },
