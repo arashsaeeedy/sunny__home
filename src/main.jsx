620dc4bfd9cdc01c244634a9a3b07cd83e50eedf
yourname <your@email.com>
2025-10-31 00:20:10 +0330

    Fix merge conflicts and update project

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import React from 'react'
+﻿import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -10,15 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 )
-=======
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
->>>>>>> 0bc683b718467269591d3468d2f129d3207db59d
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 export default defineConfig({
   plugins: [react()],
-  base: '/sunny__home/' // set this to "/<your-repo-name>/" for GitHub Pages
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/sunny__home/', // Set base for GitHub Pages
+  base: '/sunny__home/',
   server: {
     historyApiFallback: true,
   },
->>>>>>> 0bc683b718467269591d3468d2f129d3207db59d
 })
